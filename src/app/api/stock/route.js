29177210da2e2c48235ea338fb8b7695b4da11c6
Amitkumar-56 <amitk73262@gmail.com>
2025-10-10 22:41:13 +0530

    new

--- src/app/api/stock/route.js
+++ src/app/api/stock/route.js
@@ -7,20 +7,17 @@ export async function GET() {
     const query = `
    
 
-       SELECT fs.id, fs.station_name, IFNULL(fss.stock, 0) AS stock, p.pcode AS product_name
-        FROM filling_stations fs
-        LEFT JOIN filling_station_stocks fss ON fs.id = fss.fs_id
-        LEFT JOIN products p ON fss.product = p.id
+   SELECT 
+        fs.station_name,
+        p.pname AS product_name,
+        s.stock
+      FROM stock s
+      LEFT JOIN products p ON s.product_id = p.id
+      LEFT JOIN filling_stations fs ON s.fs_id = fs.id
+      ORDER BY fs.station_name ASC, p.pname ASC
     `;
 
-  //  SELECT 
-  //       fs.station_name,
-  //       p.pname AS product_name,
-  //       s.stock
-  //     FROM stock s
-  //     LEFT JOIN products p ON s.product_id = p.id
-  //     LEFT JOIN filling_stations fs ON s.fs_id = fs.id
-  //     ORDER BY fs.station_name ASC, p.pname ASC
+  
     
     const data = await executeQuery({ query });
     return NextResponse.json(data);
