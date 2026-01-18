5b344adb6c91dca1fc291ccb1ce6e47d4e495d17
Amit Kumar <amitk73262@gmail.com>
2026-01-18 21:36:54 +0530

    ui

--- src/app/nb-stock/nb-stock-history/page.jsx
+++ src/app/nb-stock/nb-stock-history/page.jsx
@@ -3,7 +3,7 @@
 
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import Sidebar from '@/components/Sidebar';
+import Sidebar from '@/components/sidebar';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Suspense, useEffect, useState } from 'react';
 
--- src/app/nb-stock/page.jsx
+++ src/app/nb-stock/page.jsx
@@ -3,7 +3,7 @@
 
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import Sidebar from '@/components/Sidebar';
+import Sidebar from '@/components/sidebar';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
